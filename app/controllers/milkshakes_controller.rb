5b23daaf128c265f27a00f45f2a1cda0be8e821d
rmar8138 <ragan.martinez@live.com>
2019-10-24 11:45:33 +1100

    Add stripe to backend

--- app/controllers/milkshakes_controller.rb
+++ app/controllers/milkshakes_controller.rb
@@ -1,8 +1,8 @@
 class MilkshakesController < ApplicationController
-  before_action :authenticate_user!
   before_action :set_milkshake, only: [:show]
   before_action :set_user_milkshake, only: [:edit, :update]
-
+  before_action :authenticate_user!
+  
   def index
     if params[:search] && !params[:search].empty?
       @milkshakes = Milkshake.where(name: params[:search])
@@ -12,6 +12,29 @@ class MilkshakesController < ApplicationController
   end
 
   def show
+    session = Stripe::Checkout::Session.create(
+      payment_method_types: ["card"],
+      customer_email: current_user.email,
+      line_items: [
+        {
+          name: @milkshake.name,
+          description: @milkshake.description,
+          amount: @milkshake.price,
+          currency: "aud",
+          quantity: 1
+        }
+      ],
+      payment_intent_data: {
+        metadata: {
+          user_id: current_user.id,
+          milkshake_id: @milkshake.id
+        }
+      },
+      success_url: "#{root_url}payment/success?userId=#{current_user.id}&milkshakeId=#{@milkshake.id}",
+      cancel_url: "#{root_url}milkshakes/#{@milkshake.id}"
+    )
+
+    @session_id = session.id
   end
 
   def new
@@ -35,7 +58,6 @@ class MilkshakesController < ApplicationController
   end
 
   def update
-
     if @milkshake.update(milkshake_params)
       redirect_to milkshake_path(params[:id])
     else
